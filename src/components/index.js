97dd439702bcc3965e31e2fe00840c1db6e35ad2
Angel Eduardo Gaxiola Javier <angel@192.168.1.6>
2024-10-02 00:06:46 -0700

    feat:start template with profile, about me, experience, projects

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export  { default as ProfileInfo } from './Profile.astro';
+export  { default as AboutMe } from './AboutMe.astro';
+export  { default as SocialLinks } from './SocialLinks.astro';
+export  { default as Experience } from './Experience.astro';
+export  { default as TechFilters } from './TechFilters.astro';
+export { default as Gallery } from './Gallery.astro';
